8a69e2cfe1686a8e65942b03c685c2018340dcdf
Alipongoy <joshuajardenil@outlook.com>
2016-04-29 21:15:25 -0700

    deleted files

--- Makefile
+++
@@ -1,5 +0,0 @@
-bb8: main.cc print.o
-	g++ -std=c++0x main.cc -o bb8
-
-print.o: print.c
-	gcc -c print.c
--- print.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-print_number(int x) {
-	printf("%i\n",x);
-}
-
-print_string(char *str) {
-	printf("%s",str);
-}
